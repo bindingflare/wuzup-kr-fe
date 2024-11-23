2b95305497145b17328fbda1131effcf033469af
junhyun bang <flintintoe@gmail.com>
2024-11-24 01:44:38 +0900

    feat: Create main page

---
+++ src/components/common/footer.tsx
@@ -0,0 +1,13 @@
+import FC from "react";
+
+const Footer: FC = () => {
+  return (
+    <section className="bg-slate-300">
+      <div className="wrapper">
+        <p>Made by WUZUP-KR team.</p>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
---
+++ src/components/common/header.tsx
@@ -0,0 +1,13 @@
+import FC from "react";
+
+const Header: FC = () => {
+  return (
+    <section className="bg-slate-300">
+      <div className="wrapper">
+        <h2>WUZUP KR</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
